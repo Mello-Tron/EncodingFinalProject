1f7cf22b3c55f6a1ee11e6ad92b9f5bec7961965
Patrick <patricktrherbert@gmail.com>
2014-11-19 22:40:40 -0800

    Trying to set up Prime Number Generation
    Not much was done

--- RSA Key Generation ASSEMBLY/Main.asm
+++ RSA Key Generation ASSEMBLY/Main.asm
@@ -6,7 +6,32 @@ INCLUDE Irvine32.inc
 
 .code
 main PROC
-    
+    xor eax, eax
+	xor ecx, ecx
+	xor edx, edx
+
+	mov eax, 2
+	push eax
+	inc ecx
+
+	mov eax, 3
+	push eax
+	inc ecx
+
+	add eax, 2	;next Number to try
+	push ecx
+L1:
+	push eax
+	xor edx, edx
+	mov ebx, ecx
+	add ebx, esp
+	div ebx
+	cmp edx, 0
+
+	pop eax
+	loop L1
+	pop ecx
+	
 exit
 main ENDP
 END main
