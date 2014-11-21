0718941153d1e79633d0115a9216b22ee7bd1eae
Patrick <patricktrherbert@gmail.com>
2014-11-20 21:24:12 -0800

    Pseudo Generator Is Working
    Needs to be cleaned up and made into a PROC

--- Pseudo Random Generator/Main.asm
+++ Pseudo Random Generator/Main.asm
@@ -1,14 +1,14 @@
 TITLE Psuedo Random Number Generator
 
 INCLUDE Irvine32.inc
-
+							;R(n) = ((R(0) * K) + S) % MAX_NUM_CHAR
 .data
-inputString BYTE "apple"
-outputString BYTE "     "
-R0 DWORD 30					;whatever TIME is
-K DWORD 20					;whatever constant we want
-S DWORD 12					;whatever constant we want
-MAX_NUM_CHAR DWORD 5		;whatever character count is
+inputString BYTE "apple&pie"
+outputString BYTE "         "
+R0 DWORD 4					;0 =< R(0) < MAX_NUM_CHAR
+K DWORD 4					;0 < K < MAX_NUM_CHAR			K-1 is divisble by all prime factors of MAX_NUM_CHAR, K-1 is multiple of 4, if MAX_NUM_CHAR is a multiple of 4
+S DWORD 7					;0 =< S < MAX_NUM_CHAR			MAX_NUM_CHAR and S are coprime
+MAX_NUM_CHAR DWORD 9		;whatever character count is
 
 .code
 main PROC
@@ -30,12 +30,27 @@ L1:
 	test edx, 11011111b					;test with " " which is 20h which is 00100000b
 	jnz ShouldWeLoop					;lets jump if there is something other than an empty space
 
-	;Let's take the next letter from inputString and put it in outputString
+	push ebx
 
+	push ecx							;Let's take the next letter from inputString and put it in outputString
+	xor ecx, ecx
+	mov cl, BYTE PTR [ebx]
+	xor ebx, ebx
+	mov bl, cl
+	pop ecx
+
+	mov [ecx], bl
 	inc esi
 
+	pop ebx
+	inc ebx
+
 ShouldWeLoop:
-	
+	cmp esi, MAX_NUM_CHAR
+	je No
+	jmp L1
+No:
+
 	;now check if esi == MAX_NUM_CHAR then {we are done} else {loop again}
 
 exit
